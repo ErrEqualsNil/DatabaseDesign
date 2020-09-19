232667d57338c78008d79a73ea0a7deb119e4eb5
XinShiQaQ <811681902@qq.com>
2020-09-20 01:51:55 +0800

    搜索商品信息页面雏形

--- CourseDesign/SearchApp/views.py
+++ CourseDesign/SearchApp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from Login.models import Commodity
 # Create your views here.
 
-def searchResult(requests):
-    searchKey = requests.GET.get('searchKey')
-    return HttpResponse(searchKey)
+def searchResult(request):
+    searchKey = request.GET.get('searchKey')
+    good_list = Commodity.objects.filter(name=searchKey)
+    goods = []
+    for good in good_list:
+        goods.append({'ID': good.ID, 'Name': good.name, 'Price': good.price, 'Description': good.description})
+    return render(request, 'SearchResult.html', {'goods': goods, 'len': len(goods)})
+
 
 def searchPage(requests):
     return render(requests, 'Search.html')
