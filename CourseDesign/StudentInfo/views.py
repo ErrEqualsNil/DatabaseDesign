23834c4ab477cb90d82246c3646ff890f5cffc88
AtWillTeam <54709287+AtWillTeam@users.noreply.github.com>
2020-09-20 01:14:04 +0800

    增加功能：个人信息中删除上架的商品

--- CourseDesign/StudentInfo/views.py
+++ CourseDesign/StudentInfo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from Login.models import Account, Commodity, User
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def InfoPage(request):
@@ -10,3 +12,11 @@ def InfoPage(request):
     for good in good_list:
         goods.append({'ID': good.ID, 'Name': good.name, 'Price': good.price, 'Description': good.description})
     return render(request, 'StudentInfo.html', {'datas': datas, 'goods': goods, 'len': len(goods)})
+
+
+def DeleteItem(request):
+    delete_list = request.POST.getlist('choose')
+    for Id in delete_list:
+        Commodity.objects.filter(ID=Id).delete()
+    messages.success(request, "删除中")
+    return HttpResponseRedirect('/studentinfo')
