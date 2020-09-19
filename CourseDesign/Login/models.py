df5bf8080c72f2785f81e8061007d8663c028594
AtWillTeam <54709287+AtWillTeam@users.noreply.github.com>
2020-09-19 23:30:42 +0800

    修复前一次提交的bug并更新一点小细节

--- CourseDesign/Login/models.py
+++ CourseDesign/Login/models.py
@@ -26,7 +26,7 @@ class Commodity(models.Model):
     ID = models.CharField(max_length=15, primary_key=True)
     name = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    Description = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
     owner = models.CharField(max_length=15)
 
 
