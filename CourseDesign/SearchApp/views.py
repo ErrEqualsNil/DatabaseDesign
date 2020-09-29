7c3be36dca66bd6c8db89f3599ff289c14655ce7
XinShiQaQ <811681902@qq.com>
2020-09-29 20:07:06 +0800

    多关键词搜索完善

--- CourseDesign/SearchApp/views.py
+++ CourseDesign/SearchApp/views.py
@@ -4,12 +4,15 @@ from Model.models import Commodity
 
 def searchResult(request):
     searchKey = request.GET.get('searchKey')
-    good_list = Commodity.objects.filter(name__icontains=searchKey, status=True)
+    keys = searchKey.split(' ')
     goods = []
+    good_list = Commodity.objects.filter(name__icontains=keys[0], status=True)
+    for key in keys[1:]:
+        good_list = good_list.filter(name__icontains=key, status=True)
     for good in good_list:
         goods.append({'ID': good.id, 'Name': good.name, 'Price': good.price,
-                      'Description': good.description, 'Owner': good.owner,
-                      'image': "https://database-design.oss-cn-beijing.aliyuncs.com/" + str(good.image)})
+                    'Description': good.description, 'Owner': good.owner,
+                    'image': "https://database-design.oss-cn-beijing.aliyuncs.com/" + str(good.image)})
     return render(request, 'SearchResult.html', {'goods': goods, 'len': len(goods), 'key': searchKey})
 
 
