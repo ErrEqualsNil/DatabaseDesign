d46cf62bdb5deb57b42115d9e832cc745a2b7edb
10439 <1043936915@qq.com>
2020-12-10 21:18:02 +0800

    新增加showInfo中的订单信息的显示（未完善）

--- CourseDesign/ShowInfo/views.py
+++ CourseDesign/ShowInfo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from Model.models import Commodity, User
+from Model.models import Commodity, User, Transaction
 # Create your views here.
 
 
@@ -10,4 +10,11 @@ def showInfo(requests):
     datas = {'ID': id, 'Name': tmp.name, 'Sex': 'Male' if tmp.isMale else 'Female', 'College': tmp.college,
              'Address': tmp.address, 'QQ': tmp.QQ, 'Tel': tmp.tel, 'Email': tmp.email}
     good_list = Commodity.objects.filter(owner=id, status=True)
-    return render(requests, 'ShowInfo.html', {'datas': datas, 'goods': good_list, 'len': len(good_list)})
+    Transaction1 = Transaction.objects.filter(seller = id)
+    Trans = []
+    for tra in Transaction1:
+        Trans.append({'ID': tra.id, 'Name': tra.commodity.name, 'Price': tra.commodity.price, 'Seller': tra.seller,
+                      'Comment': tra.comment,
+                      'Buyer': requests.session['user'], 'Description': tra.commodity.description, 'Statue': tra.status,
+                      'image': "https://database-design.oss-cn-beijing.aliyuncs.com/" + str(tra.commodity.image)})
+    return render(requests, 'ShowInfo.html', {'datas': datas, 'goods': good_list, 'trans': Trans, 'len': len(good_list)})
