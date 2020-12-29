3e0213d7d555db8d2590bfd2cfb18a391b9418bb
XinShiQaQ <46019182+XinShiQaQ@users.noreply.github.com>
2020-12-29 14:59:59 +0800

    Update views.py

--- CourseDesign/CourseDesign/views.py
+++ CourseDesign/CourseDesign/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
 def startPage(requests):
-    if 'type' not in requests.session or requests.session['type'] == 'student':
+    if 'type' not in requests.session:
+        return HttpResponseRedirect("/search")
+    elif requests.session['type'] == 'student':
         return HttpResponseRedirect("/search")
     else:
         return HttpResponseRedirect("/teachers")
